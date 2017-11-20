186fcadc9c6b6e2c99196a6553d043df88bc2e71
bodyno <411298027@qq.com>
2017-11-20 16:02:04 +0800

    optimize from node:alpine

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-'use strict';
-
-const express = require('express');
+const express = require('express')
 
 // Constants
-const PORT = 8080;
+const PORT = 8080
 
 // App
-const app = express();
+const app = express()
 app.get('/', function (req, res) {
-    res.send('Hello Docker\n');
-});
+    res.send('Hello Docker\n')
+})
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(PORT)
+console.log('Running on http://localhost:' + PORT)
